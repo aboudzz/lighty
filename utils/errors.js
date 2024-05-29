1d76b9ecd96f8f6b9df24f756b7b20adcc48449c
Aboud Z <aboud.zakaria@gmail.com>
2024-05-29 13:24:10 +0000

    fix errors are not functions

--- utils/errors.js
+++ utils/errors.js
@@ -3,19 +3,19 @@ const debug = require('debug')('debug:errors');
 
 const errors = {
 
-    BAD_REQUEST: () => createError(400, 'Bad request'),
+    BAD_REQUEST: createError(400, 'Bad request', { code: "BAD_REQUEST" }),
 
-    NOT_FOUND: () => createError(404, 'Requested entity not found'),
+    NOT_FOUND: createError(404, 'Requested entity not found', { code: "NOT_FOUND" }),
 
-    UNAUTHORIZED: () => createError(403, 'Access denied'),
+    UNAUTHORIZED: createError(403, 'Access denied', { code: "UNAUTHORIZED" }),
 
-    LINK_EXPIRED: () => createError(410, 'Requested link expired'),
+    LINK_EXPIRED: createError(410, 'Requested link expired', { code: "LINK_EXPIRED" }),
 
-    EMAIL_ALREADY_REGISTERED: () => createError(400, 'Email is already registered'),
+    EMAIL_ALREADY_REGISTERED: createError(400, 'Email is already registered', { code: "EMAIL_ALREADY_REGISTERED" }),
 
-    EMAIL_NOT_REGISTERED: () => createError(400, 'Email is not registered'),
+    EMAIL_NOT_REGISTERED: createError(400, 'Email is not registered', { code: "EMAIL_NOT_REGISTERED" }),
 
-    INCORRECT_PASSWORD: () => createError(400, 'Incorrect password'),
+    INCORRECT_PASSWORD: createError(400, 'Incorrect password', { code: "INCORRECT_PASSWORD" }),
 
     error404: (req, res, next) => {
         next(createError(404, `Requested URL: \`${req.method} ${req.url}\` not found`));
