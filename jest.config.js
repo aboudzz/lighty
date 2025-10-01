e9f63f957736e8d0505300ad70189c14daae8724
Aboud Z (Eaton) <107610226+abdulzakarya-eaton@users.noreply.github.com>
2025-10-01 09:00:32 +0000

    fix: update coveragePathIgnorePatterns in jest.config.js and sonar.exclusions in sonar-project.properties

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,11 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/jest.setup.js",
+    "/jest.config.js"
+  ],
   verbose: true,
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   forceExit: true,
